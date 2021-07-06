c2a82712b6b3a458f4495201c39cd49b5f8cecfc
Md Salman Sajib <salman@simecsystem.com>
2021-07-06 14:17:46 +0600

    Readme Added and Final Version Done

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 function generatePassword(length=8, number=false, symbol=false) {
     let returnPassword = '';
+    
+    if(number && symbol){
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789`! @#$%^&*()_-+={[}]|\:;"<,>.?/';
+    }
 
+    if(!number && symbol){
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz`! @#$%^&*()_-+={[}]|\:;"<,>.?/';
+    }
+
+    if(number && !symbol){
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    }
+
+    if(!number && !symbol){
+        var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    }
+    
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        returnPassword += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
     
     return returnPassword;
 }
